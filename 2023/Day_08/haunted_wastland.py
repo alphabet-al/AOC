f1ffbcb3eb1dff38802bb166a9f71be89c646a72
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2023-12-09 10:20:40 -0600

    revised day 8

--- 2023/Day_08/haunted_wastland.py
+++ 2023/Day_08/haunted_wastland.py
@@ -122,6 +122,53 @@ def find_zzz(dir, lst, start, part2=False):
 
     return count
 
+def find_start_nodes(dir, lst):
+    # count = 0
+    # list that stores the nodes that we have just visited, we should have the same number of nodes
+    # at the end of the for loop as we did when we begane the loop, it should break out of while loop
+    # when all the nodes in the visited list end in 'z'.  if they aren't all z, then we clear the list before then next loop
+    # visited = []
+    current = deque()   # queue that stores currently visited nodes, contains key values of adjacency list
+
+    # initialize the current queue with all keys that end in 'z'
+    for k in lst.keys():
+        if k[-1] == 'A':
+            current.append(k) 
+    return current
+    # get number of start points, count of z's at the end must match this value
+    # start_A_count = len(current)
+        
+    # while True:
+    #     index = count % len(dir) # count is indexed by 1 at the end of each while loop....this lets us find the correct move
+    #     current_copy = current.copy()   # need to make a copy because we need to traverse it and mutate it
+
+    #     z_count = 0
+    #     for current_node in current_copy:    # loop through each node in the current queue    
+    #         edges = lst[current_node]
+                                    
+    #         if dir[index] == 'L':
+    #             visited.append(current.popleft())
+    #             current.append(edges[0])
+            
+    #         else:
+    #             visited.append(current.popleft())
+    #             current.append(edges[1])
+            
+    #         if current_node[-1] == 'Z':
+    #             z_count += 1
+    #     # print(visited)
+        
+    #     if z_count == start_A_count:
+    #         break
+    #     # else:
+    #     #     visited.clear()
+
+    #     count += 1
+    # # print(visited)
+    # print(count)
+
+
+
             
 def main(data):
     directions = data[0]
@@ -138,6 +185,7 @@ def main(data):
     for nodes in start_nodes:
         end_counts.append(find_zzz(directions, adj_list, nodes, part2 = True))
     
+
     num1 = end_counts[0]
     num2 = end_counts[1]
     LCM = lcm(num1, num2)
