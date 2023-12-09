4e611ba4bf72d00bddfc942223708b9b07830e07
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2023-12-09 10:25:35 -0600

    final day 8 commit

--- 2023/Day_08/haunted_wastland.py
+++ 2023/Day_08/haunted_wastland.py
@@ -80,6 +80,8 @@ Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
 So, in this example, you end up entirely on nodes that end in Z after 6 steps.
 
 Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+
+Your puzzle answer was 7309459565207.
 '''
 
 from collections import deque
@@ -122,53 +124,14 @@ def find_zzz(dir, lst, start, part2=False):
 
     return count
 
-def find_start_nodes(dir, lst):
-    # count = 0
-    # list that stores the nodes that we have just visited, we should have the same number of nodes
-    # at the end of the for loop as we did when we begane the loop, it should break out of while loop
-    # when all the nodes in the visited list end in 'z'.  if they aren't all z, then we clear the list before then next loop
-    # visited = []
-    current = deque()   # queue that stores currently visited nodes, contains key values of adjacency list
 
-    # initialize the current queue with all keys that end in 'z'
+def find_start_nodes(dir, lst):
+    current = deque()   
     for k in lst.keys():
         if k[-1] == 'A':
             current.append(k) 
     return current
-    # get number of start points, count of z's at the end must match this value
-    # start_A_count = len(current)
-        
-    # while True:
-    #     index = count % len(dir) # count is indexed by 1 at the end of each while loop....this lets us find the correct move
-    #     current_copy = current.copy()   # need to make a copy because we need to traverse it and mutate it
-
-    #     z_count = 0
-    #     for current_node in current_copy:    # loop through each node in the current queue    
-    #         edges = lst[current_node]
-                                    
-    #         if dir[index] == 'L':
-    #             visited.append(current.popleft())
-    #             current.append(edges[0])
-            
-    #         else:
-    #             visited.append(current.popleft())
-    #             current.append(edges[1])
-            
-    #         if current_node[-1] == 'Z':
-    #             z_count += 1
-    #     # print(visited)
-        
-    #     if z_count == start_A_count:
-    #         break
-    #     # else:
-    #     #     visited.clear()
-
-    #     count += 1
-    # # print(visited)
-    # print(count)
-
-
-
+  
             
 def main(data):
     directions = data[0]
@@ -178,14 +141,12 @@ def main(data):
     # print(find_zzz(directions, adj_list, 'AAA', part2 = False))
 
     """ part 2 """
-    # find_all_z(directions, adj_list)
     start_nodes = find_start_nodes(directions, adj_list)
     end_counts = []
     
     for nodes in start_nodes:
         end_counts.append(find_zzz(directions, adj_list, nodes, part2 = True))
     
-
     num1 = end_counts[0]
     num2 = end_counts[1]
     LCM = lcm(num1, num2)
@@ -197,8 +158,8 @@ def main(data):
 
 
 if __name__ == '__main__':
-    # path =  r'C:\Users\alanv\PythonCode\Projects\AOC\2023\Day_08\test_data3.txt'
-    path =  r'C:\Users\alanv\PythonCode\Projects\AOC\2023\Day_08\data.txt'
+    # path =  r'C:\AOC\2023\Day_08\test_data2.txt'
+    path =  r'C:\AOC\2023\Day_08\data.txt'
 
     with open(path, 'r') as file:
         input = file.read().splitlines()
