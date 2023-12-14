36b780916e6abcbd5e5603a013f925f8cf1d4d35
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2023-12-13 23:38:35 -0600

    10.2 complete

--- 2023/Day_10/Pipe_Maze.py
+++ 2023/Day_10/Pipe_Maze.py
@@ -1,5 +1,211 @@
+"""
+--- Day 10: Pipe Maze ---
+You use the hang glider to ride the hot air from Desert Island all the way up to the floating metal island. This island is surprisingly cold and there 
+definitely aren't any thermals to glide on, so you leave your hang glider behind.
+
+You wander around for a while, but you don't find any people or animals. However, you do occasionally find signposts labeled "Hot Springs" pointing in a 
+seemingly consistent direction; maybe you can find someone at the hot springs and ask them where the desert-machine parts are made.
+
+The landscape here is alien; even the flowers and trees are made of metal. As you stop to admire some metal grass, you notice something metallic scurry 
+away in your peripheral vision and jump into a big pipe! It didn't look like any animal you've ever seen; if you want a better look, you'll need to get 
+ahead of it.
+
+Scanning the area, you discover that the entire field you're standing on is densely packed with pipes; it was hard to tell at first because they're the 
+same metallic silver color as the "ground". You make a quick sketch of all of the surface pipes you can see (your puzzle input).
+
+The pipes are arranged in a two-dimensional grid of tiles:
+
+| is a vertical pipe connecting north and south.
+- is a horizontal pipe connecting east and west.
+L is a 90-degree bend connecting north and east.
+J is a 90-degree bend connecting north and west.
+7 is a 90-degree bend connecting south and west.
+F is a 90-degree bend connecting south and east.
+. is ground; there is no pipe in this tile.
+S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+Based on the acoustics of the animal's scurrying, you're confident the pipe that contains the animal is one large, continuous loop.
+
+For example, here is a square loop of pipe:
+
+.....
+.F-7.
+.|.|.
+.L-J.
+.....
+If the animal had entered this loop in the northwest corner, the sketch would instead look like this:
+
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
+In the above diagram, the S tile is still a 90-degree F bend: you can tell because of how the adjacent pipes connect to it.
+
+Unfortunately, there are also many pipes that aren't connected to the loop! This sketch shows the same loop as above:
+
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+In the above diagram, you can still figure out which pipes form the main loop: they're the ones connected to S, pipes those pipes connect to, pipes 
+those pipes connect to, and so on. Every pipe in the main loop connects to its two neighbors (including S, which will have exactly two pipes connecting 
+to it, and which is assumed to connect back to those two pipes).
+
+Here is a sketch that contains a slightly more complex main loop:
+
+..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+Here's the same example sketch with the extra, non-main-loop pipe tiles also shown:
+
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+If you want to get out ahead of the animal, you should find the tile in the loop that is farthest from the starting position. Because the animal is in 
+the pipe, it doesn't make sense to measure this by direct distance. Instead, you need to find the tile that would take the longest number of steps along 
+the loop to reach from the starting point - regardless of which way around the loop the animal went.
+
+In the first example with the square loop:
+
+.....
+.S-7.
+.|.|.
+.L-J.
+.....
+You can count the distance each tile in the loop is from the starting point like this:
+
+.....
+.012.
+.1.3.
+.234.
+.....
+In this example, the farthest point from the start is 4 steps away.
+
+Here's the more complex loop again:
+
+..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+Here are the distances for each tile on that loop:
+
+..45.
+.236.
+01.78
+14567
+23...
+Find the single giant loop starting at S. How many steps along the loop does it take to get from the starting position to the point farthest from the 
+starting position?
+
+Your puzzle answer was 6828.
+
+--- Part Two ---
+You quickly reach the farthest point of the loop, but the animal never emerges. Maybe its nest is within the area enclosed by the loop?
+
+To determine whether it's even worth taking the time to search for such a nest, you should calculate how many tiles are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+The above loop encloses merely four tiles - the two pairs of . in the southwest and southeast (marked I below). The middle . tiles (marked O below) are 
+not in the loop. Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+In fact, there doesn't even need to be a full tile path to the outside for tiles to count as outside the loop - squeezing between pipes is also allowed! 
+Here, I is still within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+The above sketch has many random bits of ground, some of which are in the loop (I) and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the loop. Here's another example with many bits of junk pipe lying around 
+that aren't connected to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area within the loop. How many tiles are enclosed by the loop?
+
+Your puzzle answer was 459.
+"""
+
 from collections import deque
 import numpy as np
+import cv2
 
 
 legal = {"|" : ("north", "south"), 
@@ -44,6 +250,7 @@ def find_direction_by_value(value):
             return key
     return None
 
+
 # find coordinates for start position
 def find_start(input):
     for i, row in enumerate(input):
@@ -74,6 +281,7 @@ def moves(pos, input, visited):
                     
     return 0
 
+
 # find exit headings for start position
 def find_start_position_exits(pos, input):
 
@@ -88,14 +296,15 @@ def find_start_position_exits(pos, input):
                 start_position_exits.append(path_to)
 
 
-def bfs(start, input):
+def search(start, input):
     
     visited = []
     q.append(start)
 
     while True:
-        current_pos = q.popleft()
-        
+        # current_pos = q.popleft()   # uncomment for BFS (Part 1)  ....need this to find farthest point from start
+        current_pos = q.pop()   # uncomment for DFS (Part 2) ....need this to find a contiguous path
+
         if current_pos in visited:
             break
 
@@ -105,6 +314,7 @@ def bfs(start, input):
 
 
     return visited
+
 
 def check_adj(x, y, grid):
     new_loc = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
@@ -126,143 +336,25 @@ def check_adj(x, y, grid):
 def steps(input):
     start = find_start(input)
     find_start_position_exits(start, input)
-    answer = bfs(start, input)
+    answer = search(start, input)
     max_steps = len(answer) / 2
     
     return (int(max_steps), answer)
 
-def flood_fill(coordinates, coord_set=None, current=None):
 
-    # Initialize the set on the first call
-    if coord_set is None:
-        coord_set = set(coordinates)
+def find_number_of_inside_points(input):
+    contour = np.array(pipe_path, dtype=np.int32)
+    r,c = len(input), len(input[0])
+    inside_points = 0
 
-    # Define a function to get neighbors of a given coordinate
-    def get_neighbors(coord):
-        x, y = coord
-        # Neighbors (up, down, left, right)
-        return [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+    for i in range(r):
+        for j in range(c):
 
-    # If current is None, we start with the first coordinate
-    if current is None:
-        for coord in coordinates:
-            flood_fill(coordinates, coord_set, coord)
-    else:
-        neighbors = get_neighbors(current)
-        for neighbor in neighbors:
-            if neighbor not in coord_set:
-                coord_set.add(neighbor)
-                coordinates.append(neighbor)
-                flood_fill(coordinates, coord_set, neighbor)
-
-
-def find_area(path, grid):
-    clockwise = []
-    counter_clockwise = []
-    count = 0
+            if cv2.pointPolygonTest(contour, (i,j), False) > 0:
+                inside_points += 1
     
-    ''' for test data 1=3 and main data'''
-    for i in range(0, len(path), 2):
-        clockwise.append(path[i])
-    for i in range(1, len(path) + 1, 2):
-        counter_clockwise.append(path[i])    
+    return inside_points
 
-    ''' for test data 4 and 5'''
-    # for i in range(0, len(path), 2):
-    #     counter_clockwise.append(path[i])
-    # for i in range(1, len(path) + 1, 2):
-    #     clockwise.append(path[i])    
-
-    # print(path)
-    # print(clockwise)
-    # print(counter_clockwise)
-
-    for j in range(1):
-        inside = []
-
-        for i in range(len(clockwise) - 1):
-            cr, cc = clockwise[i]
-            nr, nc = clockwise[i+1]
-            d_vector = (nr - cr, nc - cc)
-            direction = find_direction_by_value(d_vector) # traverse direction
-            right_direction = right_side[direction] # find right side perpendicular direction of travel
-            offset = dir[right_direction]   # offset vector
-            # print(offset)
-            # take current position and add offset to inspect right side of path....if '.' then turn '.' to '1'
-            # add coordinate to visited and count up by one
-
-            dy, dx = (cr + offset[0], cc + offset[1])
-
-            if 0 <= dy < len(grid) and 0 <= dx < len(grid[0]):
-                if (dy,dx) not in path:
-                    grid[dy][dx] = 'I'
-                    path.append((dy,dx))
-                    inside.append((dy,dx))
-                    # print((dy,dx))
-                    count += 1
-
-
-
-        for i in range(len(counter_clockwise) - 1):
-            cr, cc = counter_clockwise[i]
-            nr, nc = counter_clockwise[i+1]
-            d_vector = (nr - cr, nc - cc)
-            direction = find_direction_by_value(d_vector) # traverse direction
-            left_direction = left_side[direction] # find right side perpendicular direction of travel
-            offset = dir[left_direction]   # offset vector
-            # print(offset)
-            # take current position and add offset to inspect right side of path....if '.' then turn '.' to '1'
-            # add coordinate to visited and count up by one
-
-            dy, dx = (cr + offset[0], cc + offset[1])
-            if 0 <= dy < len(grid) and 0 <= dx < len(grid[0]):
-                if (dy, dx) not in path:
-                    grid[dy][dx] = 'I'
-                    path.append((dy,dx))
-                    inside.append((dy,dx))
-                    count += 1
-
-
-        # print(len(path))
-        new_loc = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-        visited = []
-        
-        for i in range(8):
-            for x,y in inside:
-                for loc in new_loc:
-                    new_row, new_col = x + loc[0], y + loc[1]
-                    # if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
-                    # new_pt = grid[new_row][new_col]
-                    if (new_row, new_col) not in path and (new_row, new_col) not in visited:
-                        grid[new_row][new_col] = 'I'
-                        path.append((dy,dx))
-                        visited.append((new_row, new_col))
-                        count += 1
-                        
-            # print(len(visited))
-            # print(len(path))
-            
-            inside = inside + visited
-
-        # for i in grid:
-        #     print(i)
-
-    print(count)
-
-def find_inside(path,grid):
-    
-    for i in grid:
-            print(i)
-    
-    grid = np.array(grid)
-    print(grid.shape)
-
-    arr = [(x,y) in path for x in range(grid.shape[0]) for y in range(grid.shape[1])]
-    arr = np.array(arr).reshape(grid.shape)    
-    print(arr)
-
-
- 
 
 if __name__ == '__main__':
 
@@ -270,30 +362,13 @@ if __name__ == '__main__':
     path = r'C:\AOC\2023\Day_10\data.txt'
 
     with open(path, 'r') as file:
-        input = [char for char in file.read().splitlines()]
+        input = [char for char in file.read().splitlines()] # part 1 input
+        input2 = [list(i) for i in input]   # part 2 input
 
-        # for i in input:
-            # print(i)
-        
-        input2 = [list(i) for i in input]
-
+    """ part 1 """
     answer, pipe_path = steps(input)
     # print( answer )
-    
-    # array = np.ones((len(input), len(input[0])))
 
-    # for coord in pipe_path:
-    #     # Check if the coordinates are within the bounds of the array
-    #     if 0 <= coord[0] < array.shape[0] and 0 <= coord[1] < array.shape[1]:
-    #         array[coord[0], coord[1]] = 0
-
-    # print(array)
-    
-    
-    find_area(pipe_path, input2)
-
-    # find_inside(pipe_path, input2)
-
-
-
-
+    """ part 2 """
+    ipts = find_number_of_inside_points(input)
+    print(ipts)
