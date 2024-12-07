2f8d23e5919ed1b9321f16b4db444513aa2330c4
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2024-12-06 19:21:29 -0600

    day6 commit

--- 2024/day_06/main2.py
+++ 2024/day_06/main2.py
@@ -10,48 +10,52 @@ def parse(grid):
 def in_bounds(curr_pos):
     return 0 <= curr_pos[0] < len(grid) and 0 <= curr_pos[1] < len(grid[0])
 
-def pathfind(grid, start, dir):
+def pathfind(grid, start):
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    curr_dir = dir
+    curr_dir = 0
     curr_pos = start
     # path is a set so we don't duplicate coordinates we've traveled to 
     path = set()
     
     while in_bounds(curr_pos):
         # add current position to path
-        path.add(curr_pos)
+        path.add((curr_pos, curr_dir))
+        
         # get coordinate of next position in direction of travel
         newR = curr_pos[0] + directions[curr_dir][0]
         newC = curr_pos[1] + directions[curr_dir][1]
+        
+        # if not in bounds, the guard has left the map
+        if not in_bounds((newR, newC)):
+            return 0
+
         # if not blocked, update current position to next position
-        if not in_bounds((newR, newC)) or grid[newR][newC] == "." or grid[newR][newC] == '^':
+        if grid[newR][newC] == "." or grid[newR][newC] == '^':
             curr_pos = (newR, newC)
+
         # check if next position is blocked or not
         # if blocked, change direction and continue loop
         else:
             curr_dir = (curr_dir + 1) % 4
+
+        if (curr_pos, curr_dir) in path:
+            return 1
     
-    return path
 
 
-def visualize_path(path, grid):
-    pathed_grid = copy.deepcopy(grid)
-
-    for r,c in path:
-        pathed_grid[r][c] = 'X'
-    
-    for row in pathed_grid:
-        print(row)
-
-
-def main(grid):
+def main(grid, pt2 = False):
     start = parse(grid)   
-    path = pathfind(grid, start, 0)
-    # visualize_path(path, grid)
-    
-    return len(path)
-    
+    count = 0
 
+    for i, row in enumerate(grid):
+        for j, col in enumerate(row):
+            if grid[i][j] == '.':
+                grid[i][j] = '#'
+                count += pathfind(grid, start)
+                grid[i][j] = '.'
+
+    return count
+    
 if __name__ == "__main__":
     # path = r"C:\AOC\2024\day_06\test_data.txt"
     path = r"C:\AOC\2024\day_06\data.txt"
